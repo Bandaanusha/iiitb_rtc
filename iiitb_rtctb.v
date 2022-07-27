8ca548b713acc89bcf41606838fe6c8975390eae
Bandaanusha <62790565+Bandaanusha@users.noreply.github.com>
2022-07-27 10:13:00 +0530

    Update iiitb_rtctb.v

--- iiitb_rtctb.v
+++ iiitb_rtctb.v
@@ -1,1 +1,17 @@
+module test;
+reg clk,rst;
+wire [6:0] HR_M,HR_L,MIN_M,MIN_L,SEC_M,SEC_L;
+RTDC dut(rst, clk, SEC_M,SEC_L,MIN_M,MIN_L,HR_M,HR_L);
+initial begin
+clk=0;
+rst=1;
+#10 rst=0;
+#500 Sfinish();
+end
+always #10 clk=~clk;
+initial begin
+Sdumpfile("dump.vcd");
+Sdumpvars;
+end
 
+endmodule
