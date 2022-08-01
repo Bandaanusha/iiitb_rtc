fae20d85b4893c7306a4f2d282ebcb7e2d960c4a
Bandaanusha <62790565+Bandaanusha@users.noreply.github.com>
2022-08-01 18:45:46 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -11,4 +11,4 @@ abc -liberty /home/anusha/iiitb_rtc/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 clean
 flatten
 # write synthesized design
-write_verilog -assert iiitb_rtc_synth.v
+write_verilog -noattr iiitb_rtc_synth.v
