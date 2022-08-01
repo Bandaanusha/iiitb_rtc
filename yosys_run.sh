0dce53d2c1becded5822ea884c61c73193c3e6dd
Bandaanusha <62790565+Bandaanusha@users.noreply.github.com>
2022-08-01 11:51:27 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -6,8 +6,8 @@ read_verilog iiitb_rtc.v
 synth -top iiitb_rtc
 
 # mapping to mycells.lib
-dfflibmap -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
-abc -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
+dfflibmap -liberty /home/anusha/iiitb_rtc/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/anusha/iiitb_rtc/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 clean
 flatten
 # write synthesized design
