a749ccfb90bb1f9c777e0335c32d8f5dea6bc23b
Bandaanusha <62790565+Bandaanusha@users.noreply.github.com>
2022-08-01 12:03:49 +0530

    Update iiitb_rtc_tb.v

--- iiitb_rtc_tb.v
+++ iiitb_rtc_tb.v
@@ -1,20 +1,15 @@
-`timescale 1s/1ms
+`timescale 1ns/1ps
 module iiitb_rtc_tb;
- reg clk,rst;
- wire [3:0]hrm,hrl,minm,minl,secm,secl;
-
- iiitb_rtc r1(.clk_1hz(clk),.rst(rst),.hrm(hrm),.hrl(hrl),.minm(minm),.minl(minl),.secm(secm),.secl(secl)); 
- 
- always #10 clk=~clk;
-
- initial begin
-
-  $dumpfile("iiitb_rtc_out.vcd");
-  $dumpvars(0,iiitb_rtc_tb);
-  clk=0;rst=0;
-  #2 rst=1;
-  #99980 rst=0;
-  
-  #100 $finish;
- end
+reg clk,rst;
+wire [3:0]hrm,hrl,minm,minl,secm,secl;
+iiitb_rtc r1(.clk_1hz(clk),.rst(rst),.hrm(hrm),.hrl(hrl),.minm(minm),.minl(minl),.secm(secm),.secl(secl)); 
+always #10 clk=~clk;
+initial begin
+$dumpfile("iiitb_rtc_out.vcd");
+$dumpvars(0,iiitb_rtc_tb);
+clk=0;rst=0;
+#2 rst=0;
+#99980 rst=1;
+#100 $finish;
+end
 endmodule
