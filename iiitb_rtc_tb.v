11b3632d475493ed43ab9cc498df07daca532c89
Bandaanusha <62790565+Bandaanusha@users.noreply.github.com>
2022-08-17 12:06:37 +0530

    Update iiitb_rtc_tb.v

--- iiitb_rtc_tb.v
+++ iiitb_rtc_tb.v
@@ -4,11 +4,14 @@ reg clk,rst;
 wire [3:0]hrm,hrl,minm,minl,secm,secl;
 iiitb_rtc r1(.clk(clk),.rst(rst),.hrm(hrm),.hrl(hrl),.minm(minm),.minl(minl),.secm(secm),.secl(secl)); 
 initial begin
+clk=0;rst=0;
 $dumpfile("iiitb_rtc_out.vcd");
 $dumpvars(0,iiitb_rtc_tb);
-clk=0;rst=0;
-#2 rst=0;
+forever
+#10 clk=~clk;
+end
+initial begin
+#40 rst=1;
 #99980 rst=1;
 end
-always #10 clk=~clk;
 endmodule
