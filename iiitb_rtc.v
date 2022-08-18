2098797f5525eca8f080cd6b0f2320a638c6b94f
Bandaanusha <62790565+Bandaanusha@users.noreply.github.com>
2022-08-18 19:12:20 +0530

    Update iiitb_rtc.v

--- iiitb_rtc.v
+++ iiitb_rtc.v
@@ -1,25 +1,24 @@
 module iiitb_rtc (
-input clk,rst,
+input clkin,rst,
 output [3:0]hrm,hrl,minm,minl,secm,secl);
 wire [3:0]hrms,hrls,minms,minls,secms,secls;
 wire hrclr;
-wire hundred_clk;
 assign hrclr = ((secl==4'd9) && (secm==4'd5) && (minl==4'd9) && (minm==4'd5) && (hrl==4'd3) && (hrm==4'd2));
-clock_div cd(clk,rst,hundred_clk);
-counter #(9) c1(.hundred_clk(hundred_clk),.rst(rst),.clr(1'b0),.en(1'b1),.count(secl));
-counter #(5) c2(.hundred_clk(hundred_clk),.rst(rst),.clr(1'b0),.en(secl==4'd9),.count(secm));
-counter #(9) c3(.hundred_clk(hundred_clk),.rst(rst),.clr(1'b0),.en((secl==4'd9) && (secm==4'd5)),.count(minl));
-counter #(5) c4(.hundred_clk(hundred_clk),.rst(rst),.clr(1'b0),.en((secl==4'd9) && (secm==4'd5) && (minl==4'd9)),.count(minm));
-counter #(9) c5(.hundred_clk(hundred_clk),.rst(rst),.clr(hrclr),.en((secl==4'd9) && (secm==4'd5) && (minl==4'd9) && (minm==4'd5)),.count(hrl));
-counter #(2) c6(.hundred_clk(hundred_clk),.rst(rst),.clr(hrclr),.en((secl==4'd9) && (secm==4'd5) && (minl==4'd9) && (minm==4'd5) && (hrl==4'd9)),.count(hrm));
+//clock_div cd(clk,rst,clk);
+  counter #(9) c1(.clk(clkin),.rst(rst),.clr(1'b0),.en(1'b1),.count(secl));
+  counter #(5) c2(.clk(clkin),.rst(rst),.clr(1'b0),.en(secl==4'd9),.count(secm));
+  counter #(9) c3(.clk(clkin),.rst(rst),.clr(1'b0),.en((secl==4'd9) && (secm==4'd5)),.count(minl));
+  counter #(5) c4(.clk(clkin),.rst(rst),.clr(1'b0),.en((secl==4'd9) && (secm==4'd5) && (minl==4'd9)),.count(minm));
+  counter #(9) c5(.clk(clkin),.rst(rst),.clr(hrclr),.en((secl==4'd9) && (secm==4'd5) && (minl==4'd9) && (minm==4'd5)),.count(hrl));
+  counter #(2) c6(.clk(clkin),.rst(rst),.clr(hrclr),.en((secl==4'd9) && (secm==4'd5) && (minl==4'd9) && (minm==4'd5) && (hrl==4'd9)),.count(hrm));
 endmodule
 
 module counter #(parameter max_value=15) (
-input hundred_clk,rst,clr,en,
+input clk,rst,clr,en,
 output reg [3:0] count);
 initial
 count=4'b0;
-always @(posedge hundred_clk)
+always @(posedge clk)
 begin
 if(rst==0) count <=4'd0;
 else if(clr==1) count<=4'd0;
@@ -33,7 +32,7 @@ end
 end
 endmodule
 
-module clock_div(ClkIn,rst,Clk100);
+/*module clock_div(ClkIn,rst,Clk100);
 input ClkIn;
 input rst;
 output reg Clk100;
@@ -65,3 +64,4 @@ else
 clk_div100 <= clk_div100 + 19'b1;
 end
 endmodule
+*/
